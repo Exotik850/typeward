c2fe5e1ad20276fd7ab9b7b53e7bd2dfa4f3fb4e
kidkool850@gmail.com <kidkool850@gmail.com>
2026-04-15 18:01:20 -0500

    refactor Rest, Eof, and Fail parsers for improved clarity and consistency

--- src/primitives/basic.rs
+++ src/primitives/basic.rs
@@ -1,27 +1,47 @@
+use std::borrow::Cow;
+
+use crate::{input::Input, parse::Parse};
+
 pub type Empty = ();
 
 /// A parser that always succeeds and consumes the rest of the input.
-#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Default)]
 pub struct Rest<I> {
     value: I,
 }
 
-impl<'a, I> crate::parse::Parse<'a, I> for Rest<I>
+impl<'a, I> Parse<'a, I> for Rest<I>
 where
-    I: crate::input::Input<'a>,
+    I: Input<'a>,
 {
     fn parse(value: I) -> crate::error::ParseResult<(Self, I)> {
         Ok((Rest { value }, I::empty()))
     }
 }
 
+impl<I> std::ops::Deref for Rest<I> {
+    type Target = I;
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+impl<I: PartialEq<I>> PartialEq<I> for Rest<I> {
+    fn eq(&self, other: &I) -> bool {
+        &self.value == other
+    }
+}
+
+pub type RestStr<'a> = Rest<&'a str>;
+pub type RestString = Rest<String>;
+pub type RestCowStr<'a> = Rest<Cow<'a, str>>;
+
 /// A parser that checks that the input is empty, and fails if it is not.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Eof;
 
-impl<'a, I> crate::parse::Parse<'a, I> for Eof
+impl<'a, I> Parse<'a, I> for Eof
 where
-    I: crate::input::Input<'a>,
+    I: Input<'a>,
 {
     fn parse(input: I) -> crate::error::ParseResult<(Self, I)> {
         if input.is_empty() {
@@ -39,11 +59,28 @@ where
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Fail;
 
-impl<'a, I> crate::parse::Parse<'a, I> for Fail
+impl<'a, I> Parse<'a, I> for Fail
 where
-    I: crate::input::Input<'a>,
+    I: Input<'a>,
 {
     fn parse(_: I) -> crate::error::ParseResult<(Self, I)> {
         Err(crate::error::custom("Expected Fail to always fail"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{and, parse::Parse, primitives::prelude::AlphaString};
+
+    use super::*;
+
+    #[test]
+    fn test_rest() {
+        let input = "some input";
+        type AlphaAndRest<'a> = and!(AlphaString, RestStr<'a>);
+        let (res, remain) = AlphaAndRest::parse(input).unwrap();
+        assert_eq!(res.left, "some");
+        assert_eq!(res.right.value, " input");
+        assert_eq!(remain, "");
     }
 }
