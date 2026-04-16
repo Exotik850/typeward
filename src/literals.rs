e606fb391a4c7a42e7070cc81737c90eb2e8299f
kidkool850@gmail.com <kidkool850@gmail.com>
2026-04-15 19:11:31 -0500

    Add helper macro for single-character literal tokens

--- src/literals.rs
+++ src/literals.rs
@@ -76,3 +76,24 @@ define_tokens!(
     /// The ` ` space character token.
     Space, " ";
 );
+
+/// A helper macro for defining single-character tokens, which can be parsed directly from the input without needing to match a longer string.
+macro_rules! define_char_token {
+    ($($ch:tt),*) => {
+        $(
+            pastey::paste! {
+                define_tokens!(
+                    #[doc = "The `" $ch "` character token."]
+                    #[allow(non_camel_case_types)]
+                    [<$ch:replace("\"", "") Char>], $ch;
+                );
+            }
+        )*
+    };
+}
+
+define_char_token!(
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+);
