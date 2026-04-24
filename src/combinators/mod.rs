1aee3680eb0c1ba467ac12d47b56a4d4cd13d791
Exotik850 <kidkool850@gmail.com>
2026-04-24 10:54:57 -0500

    fmt, fix alpha string test

--- src/combinators/mod.rs
+++ src/combinators/mod.rs
@@ -1,24 +1,24 @@
 pub mod and;
+pub mod and_is;
 pub mod delim_nested;
 pub mod delimited;
+pub mod ignore;
 pub mod not;
 pub mod or;
 pub mod peek;
 pub mod separated;
 pub mod span;
 pub mod ws;
-pub mod ignore;
-pub mod and_is;
 
 pub mod prelude {
     pub use super::and::And;
     pub use super::delim_nested::{Braced, Bracketed, DelimNested, Parenthesized};
     pub use super::delimited::{Delimited, DelimitedExact, Padded, PaddedExact};
+    pub use super::ignore::{Forget, Ignore, Preceded, Terminated};
     pub use super::not::Not;
     pub use super::or::{Either, Or};
     pub use super::peek::Peek;
     pub use super::separated::{CommaSeparated, CommaSeparated0, Separated, Separated0};
     pub use super::span::{Span, SpanExt};
     pub use super::ws::{Ws, WsExt};
-    pub use super::ignore::{Ignore, Forget, Preceded, Terminated};
 }
