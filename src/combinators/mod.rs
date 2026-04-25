41f1029814b533fe4754745096799539ea67a759
kidkool850@gmail.com <kidkool850@gmail.com>
2026-04-25 02:08:59 -0500

    add exact separated types to prelude

--- src/combinators/mod.rs
+++ src/combinators/mod.rs
@@ -19,14 +19,15 @@ pub mod prelude {
     pub use super::cut::{Commit, Cut};
     pub use super::delim_nested::{Braced, Bracketed, DelimNested, Parenthesized};
     pub use super::delimited::{Delimited, DelimitedExact, Padded, PaddedExact};
-    pub use super::ignore::{Forget, Ignore, Preceded, Terminated, Between};
+    pub use super::ignore::{Between, Forget, Ignore, Preceded, Terminated};
     pub use super::keyword::{IdentBoundary, Keyword, KeywordBoundary, Kw};
     pub use super::left_assoc::LeftAssoc;
     pub use super::not::Not;
     pub use super::or::{Either, Or};
     pub use super::peek::Peek;
     pub use super::separated::{
-        CommaSeparated, CommaSeparated0, Separated, Separated0, SeparatedIter,
+        CommaSeparated, CommaSeparated0, Separated, Separated0, SeparatedExact, SeparatedExact0,
+        SeparatedIter,
     };
     pub use super::span::{Span, SpanExt};
     pub use super::ws::{Ws, WsExt};
